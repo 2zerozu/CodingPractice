4fd844d44227dd9a49acb7a3f847d9b5d01ba32d
이영주 <yungju9982@naver.com>
2022-06-17 05:53:08 +0900

    1066

---
+++ CodeUp/1066.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 3
+
+int main(void) {
+    int *arr = (int*)malloc(sizeof(int) * SIZE);
+    for(int i=0;i<SIZE;i++) scanf("%d", &arr[i]);
+    for (int i=0;i<SIZE;i++){
+        if(arr[i]%2==0) printf("even\n");
+        else printf("odd\n");
+    }
+    
+    return 0;
+}
